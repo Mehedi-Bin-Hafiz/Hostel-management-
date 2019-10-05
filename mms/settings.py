528bd7c0439332b60fd9e93da2aea0802c59d4bd
Mehedi-Bin-Hafiz <mehedibinhafiz@gmail.com>
2019-10-05 17:44:55 +0600

    database update

--- mms/settings.py
+++ mms/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
